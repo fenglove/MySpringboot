89340a7872022ecb0ff92e51cb5c73c9dacd8f0a
solofeng <lf178178@163.com>
2019-05-09 02:51:10 +0800

    完善spring boot整合redis

--- chapter8/src/main/java/com/itcpay/chapter/entity/User.java
+++ chapter8/src/main/java/com/itcpay/chapter/entity/User.java
@@ -1,16 +1,20 @@
 package com.itcpay.chapter.entity;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class User implements Serializable {
 
     private Long id;
+    @NonNull
     private String username;
+    @NonNull
     private String password;
 
 }
--- chapter8/src/test/java/com/itcpay/chapter/Chapter8ApplicationTests.java
+++ chapter8/src/test/java/com/itcpay/chapter/Chapter8ApplicationTests.java
@@ -1,7 +1,10 @@
 package com.itcpay.chapter;
 
+import com.itcpay.chapter.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -17,6 +20,8 @@ import java.util.stream.IntStream;
 @SpringBootTest
 public class Chapter8ApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(Chapter8ApplicationTests.class);
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -25,8 +30,21 @@ public class Chapter8ApplicationTests {
 
     @Test
     public void contextLoads() {
+        // 测试线程安全
         ExecutorService executorService = Executors.newFixedThreadPool(1000);
-        IntStream.range(0, 1000).forEach(i -> executorService.execute(() -> stringRedisTemplate));
+        IntStream.range(0, 1000).forEach(i ->
+                executorService.execute(() -> stringRedisTemplate.opsForValue().increment("kk", 1)));
+
+        stringRedisTemplate.opsForValue().set("k1", "v1");
+        String k1 = stringRedisTemplate.opsForValue().get("k1");
+        log.info("字符缓存结果-{}", k1);
+
+        // 以下只演示整合，具体Redis命令可以参考官方文档
+        String key = "itcpay:user:1";
+        redisCacheTemplate.opsForValue().set(key, new User("u1", "p1"));
+
+        User user = (User) redisCacheTemplate.opsForValue().get(key);
+        log.info("对象缓存结果-{}", user);
     }
 
 }
