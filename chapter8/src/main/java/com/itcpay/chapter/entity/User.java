264530300360ccb9dd968cd9f2fc06bbfa6ad29a
solofeng <feng.liu@alavening.com>
2019-05-08 19:03:24 +0800

    spring boot 整合redis

---
+++ chapter8/src/main/java/com/itcpay/chapter/entity/User.java
@@ -0,0 +1,16 @@
+package com.itcpay.chapter.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+public class User implements Serializable {
+
+    private Long id;
+    private String username;
+    private String password;
+
+}
---
+++ chapter8/src/test/java/com/itcpay/chapter/Chapter8ApplicationTests.java
@@ -0,0 +1,32 @@
+package com.itcpay.chapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.Serializable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter8ApplicationTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Serializable> redisCacheTemplate;
+
+    @Test
+    public void contextLoads() {
+        ExecutorService executorService = Executors.newFixedThreadPool(1000);
+        IntStream.range(0, 1000).forEach(i -> executorService.execute(() -> stringRedisTemplate));
+    }
+
+}
