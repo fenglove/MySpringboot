916e149b251a804784512bdf4b9b0533aad68448
solofeng <feng.liu@alavening.com>
2019-05-10 11:23:33 +0800

    学习Spring Bootd定时任务

---
+++ chapter12/src/main/java/com/itcpaya/chapter/task/SpringTaskDemo.java
@@ -0,0 +1,42 @@
+package com.itcpaya.chapter.task;
+
+import org.apache.tomcat.jni.Local;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 基于 Spring 自带的
+ * @since 2019-5-10 10:53:18
+ * @author solofeng
+ */
+@Component
+public class SpringTaskDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringTaskDemo.class);
+
+    // 秒 分钟 小时 天 月 周
+    @Async
+    @Scheduled(cron = "0/1 * * * * *")
+    public void scheduled1() throws InterruptedException {
+        Thread.sleep(3000);
+        log.info("scheduled1 每1秒执行一次：{}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduled2() throws InterruptedException {
+        Thread.sleep(3000);
+        log.info("scheduled2 每1秒执行一次：{}", LocalDateTime.now());
+    }
+
+    @Scheduled(fixedDelay = 3000)
+    public void scheduled3() throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("scheduled3 上次执行完毕后隔3秒继续执行：{}", LocalDateTime.now());
+    }
+
+}
